810e20d4a6e7c2a58c4249db80f3f92b38fface0
Anuj Shaan <51690694+anujshaan@users.noreply.github.com>
2021-08-27 19:59:07 +0530

    backend added

--- Backend/Server/Routes/Auth.js
+++ Backend/Server/Routes/Auth.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const User = require("../Modals/User");
+const bcrypt = require("bcrypt");
 
 //Register a user
 router.post("/register",async(req,res)=>{
     try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPass,
         })
         const user = await newUser.save();
         res.status(200).json(user);
@@ -16,9 +19,15 @@ router.post("/register",async(req,res)=>{
     }
 })
 
-
-
-
 //Login a user
+router.post("/login",async(req,res)=>{
+    try{
+        const username = req.body.username;
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 
 module.exports = router
